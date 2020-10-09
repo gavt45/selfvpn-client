2e85aa17c9a4abd24adaf2c59c29648918f152ed
ivankhramov <ivan.khramov.2002@yandex.ru>
2020-10-09 15:54:57 +0300

    update functions

--- client.py
+++ client.py
@@ -12,83 +12,79 @@ import http.client
 def register(url):
 	url = url + "/register"
 	res = requests.post(url)
-	selfvpn_conf = open('selfvpn.conf','w')
+	selfvpn_conf = open("../selfvpn.conf","w")
 
 	if res.json()['code'] == 0:
 		print("You autorized",file=sys.stdout)
-		selfvpn_conf.write(json.dumps({"uid":res.json()["uid"],"token":res.json()["token"]}))
+		selfvpn_conf.write(json.dumps({"uid":res.json()["uid"],"token":res.json()["token"],"ip":"","port":""}))
 	else:
 		print(res.json()['msg']['name']+': ' + res.json()['msg']['description'],file=sys.stderr)
 	selfvpn_conf.close()
 
 
-def push(url,port):
-	selfvpn_conf = open('selfvpn.conf')
-	dictt = json.loads(g.read())
+def push(url,s_self):
+	dictt = s_self
 	data = {
 		"uid":dictt["uid"],
 		"token":dictt["token"],
-		"port":port
+		"port":dictt["port"]
 	}
 	url = url + "/push"
 	res = requests.post(url,data=data)
-	selfvpn_conf.close()
 
 
-def update(url,client):
-	client_ovpn = open("/root/"+client+".ovpn")
-	selfvpn_conf = open("~/selfvpn.conf")
-	dictt = json.loads(selfvpn_conf.read())
+def update(url,client,s_cli,s_self):
+	dictt = s_self
 	data = {
 		"uid":dictt["uid"],
 		"token":dictt["token"],
-		"config":encode(client_ovpn.read())
+		"config":encode(s_cli)
 	}
 	url = url + "/update"
 	res = requests.post(url,data=data)
-	client_ovpn.close()
-	selfvpn_conf.close()
 
 #Changing config files
 
-def addconf(client):
-	client += ".ovpn"
-	client_ovpn = open("/root/"+client)
-	selfvpn_conf = open("~/selfvpn.conf")
-
-	s = client_ovpn.read()
-	port = s.split()[8]
+def addconf(client,s_cli,s_self):
+	s = s_cli
+	port_in_config = s.split()[7]
 	ip_in_config = s.split()[6]
-	dictt = json.loads(selfvpn_conf.read())
-	dictt.update({"ip":ip_in_config,"port":port})
-	selfvpn_conf.close()
+	dictt = s_self
+	dictt["ip"] = ip_in_config
+	dictt["port"] = port_in_config
 
-	selfvpn_conf = open("selfvpn.conf","w")
+	selfvpn_conf = open("../selfvpn.conf","w")
 	selfvpn_conf.write(json.dumps(dictt))
-
-	client_ovpn.close()
 	selfvpn_conf.close()
 
 
-def change_ip(client,ip):
-	f = open("/root/"+client+".ovpn")
-	s = f.read().split()
+def changeconf(client,ip,port,s_cli):
+	s = s_cli.split("\n")
+	s[3] = "remote " + ip + " " + port
+	f = open("/root/"+client+".ovpn","w")
+	s  = "\n".join(s)
+	f.write(s)
 	f.close()
+	return s
+
+'''
+def change_ip(client,ip,s_cli):
+	s = s_cli.split(" ")
 	s[6] = ip
-	f = open("/root/"+client,"w")
+	f = open("/root/"+client+".ovpn","w")
+
 	f.write(" ".join(s))
 	f.close()
 
 
-def chahge_port(client,port):
-	f = open("/root/"+client+".ovpn")
-	s = f.read().split()
-	f.close()
+def change_port(client,port,s_cli):
+	s = s_cli.split(" ")
+	s[7] = ""
 	s[8] = port
-	f = open("/root/"+client,"w")
+	f = open("/root/"+client+".ovpn","w")
 	f.write(" ".join(s))
 	f.close()
-
+'''
 #Secondary functions
 
 def encode(s):
@@ -107,19 +103,29 @@ def get_ip():
 url = "http://127.0.0.1:5000"
 client = "client"
 
-if not os.path.exists("~/selfvpn.conf"):
+if not os.path.exists("../selfvpn.conf"):
 	ip = get_ip()
 	port = input("input your forwarding port\n")
 	register(url)
-	change_ip(client,ip)
-	change_port(client,port)
-	addconf(client)
+
+	client_ovpn = open("/root/"+client+".ovpn")
+	selfvpn_conf = open("../selfvpn.conf")
+
+	s_cli = client_ovpn.read()
+	s_self = selfvpn_conf.read()
+
+	client_ovpn.close()
+	selfvpn_conf.close()
+
+	s_cli = changeconf(client,ip,port,s_cli)
+	addconf(client,s_cli,s_self)
+
 	print("regiter ok")
 
 while(True):
 
 	client_ovpn = open("/root/"+client+".ovpn")
-	selfvpn_conf = open("selfvpn.conf")
+	selfvpn_conf = open("../selfvpn.conf")
 
 	s_cli = client_ovpn.read()
 	s_self = json.loads(selfvpn_conf.read())
@@ -127,17 +133,18 @@ while(True):
 	client_ovpn.close()
 	selfvpn_conf.close()
 
-	ip = get_ip()
+	ip = "22.2.2.2"#get_ip()
 	ip_in_my_config = s_self["ip"]
 	ip_in_config = s_cli.split()[6]
+
 	port_in_my_config = s_self["port"]
-	port_in_config = s_cli.split()[8]
+	port_in_config = s_cli.split()[7]
 
 	if ip != ip_in_config:
-		change_ip(client,ip)
+		changeconf(client,ip,port_in_config,s_cli)
 
 	if ip_in_config != ip_in_my_config or port_in_config != port_in_my_config:
-		addconf(client)
-		push(url,port_in_my_config)
-		update(url,client)
+		addconf(client,s_cli,s_self)
+		push(url,s_self)
+		update(url,client,s_cli,s_self)
 
